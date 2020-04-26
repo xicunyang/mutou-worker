097074f5a1fda29cf3d57ed00eb34b04530bf83c
杨喜存 <xicunyang@163.com>
2020-04-26 11:35:31 +0800

    bugfix: 增加publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,3 @@
-const proxyTarget = process.env.VUE_APP_BASE_URL;
 module.exports = {
     publicPath: 'https://bj-mutou-1301404888.cos.ap-beijing.myqcloud.com/cdn/mutou-worker',
-    devServer: {
-        open: true, //浏览器自动打开页面
-        https: false,
-        hotOnly: false, //热更新（webpack已实现了，这里false即可）
-        proxy: {
-            //配置跨域
-            '/api': {
-                target: proxyTarget+"/mutou",
-                ws:true,
-                changOrigin:true,
-                pathRewrite:{
-                    '^/api':'/'
-                }
-            }
-        }
-    }
 }
